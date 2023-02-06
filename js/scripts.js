1eba9d4cbc17814f5f0038866b8f40a03d658ed8
bleaknarrative <124403331+bleaknarrative@users.noreply.github.com>
2023-02-06 17:29:54 +0100

    bleak008

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,29 @@ function showResidents() {
     x.style.display = "block";
   }
 }
+
+function showResidentImage() {
+  var span = document.querySelectorAll('.residents-container');
+for (var i = span.length; i--;) {
+    (function () {
+        var t;
+        span[i].onmouseover = function () {
+            hideAll();
+            clearTimeout(t);
+            this.className = 'residents-hover';
+        };
+        span[i].onmouseout = function () {
+            var self = this;
+            t = setTimeout(function () {
+                self.className = 'residents-container';
+            }, 300);
+        };
+    })();
+}
+
+function hideAll() {
+    for (var i = span.length; i--;) {
+        span[i].className = 'residents-container'; 
+    }
+};
+}
