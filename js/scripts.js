1c2ff3ff0aabebcd498781422ed8f30633711c70
bleaknarrative <124403331+bleaknarrative@users.noreply.github.com>
2023-02-12 17:30:41 +0100

    bleak033

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,30 @@ function showParties() {
   }
 }
 
+function showParties() {
+  var x = document.getElementById("parties-show");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
 function showResidents() {
   var x = document.getElementById("artists-hide");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
+  }
+}
+
+function showResidents() {
+  var x = document.getElementById("artists-show");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
 }
 
